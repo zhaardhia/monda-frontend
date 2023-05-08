ec5582ecc22dc3a57de053488cfb241fd3a9dd86
fadlirizaldy <fadli.rizaldi12@gmail.com>
2023-05-08 07:13:42 +0700

    tambah pages admin

---
+++ src/components/LayoutAdmin.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SidebarAdmin from "./SidebarAdmin";
+
+const LayoutAdmin = () => {
+   return (
+      <>
+         <SidebarAdmin />
+      </>
+   );
+};
+
+export default LayoutAdmin;
---
+++ src/pages/admin/index.jsx
@@ -0,0 +1,12 @@
+import LayoutAdmin from "@/components/LayoutAdmin";
+import React from "react";
+
+const AdminIndex = () => {
+   return (
+      <LayoutAdmin>
+         <div>Hello</div>
+      </LayoutAdmin>
+   );
+};
+
+export default AdminIndex;
