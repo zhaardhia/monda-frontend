a77a546f29699451388b4d6362bef7dcf88e599c
fadlirizaldy <fadli.rizaldi12@gmail.com>
2023-05-15 01:32:48 +0700

    add detail product page

--- src/components/AdminPage/CatalogueCardAdmin.jsx
+++ src/components/AdminPage/CatalogueCardAdmin.jsx
@@ -5,6 +5,8 @@ import { useRouter } from "next/router";
 import { useSessionUser } from "../../contexts/SessionUserContext";
 
 const CatalogueCardAdmin = ({ data }) => {
+  const router = useRouter();
+
   const [countCart, setCountCart] = useState(data?.userCart?.quantity || 0);
   const { axiosJWT, refreshToken, dispatch, state } = useSessionUser();
 
@@ -32,25 +34,16 @@ const CatalogueCardAdmin = ({ data }) => {
     <div className="w-64 shadow-xl rounded-lg flex flex-col items-center">
       <img src="/sambel-roa.png" alt="" className="md:w-[18rem] w-[5rem] object-cover" />
       <div className="w-[90%] my-4 flex flex-col gap-2">
-        <Link href={`/shop/detail-product/${data?.id}`} className="text-xl font-semibold">
+        <Link href={`product/detail`} className="text-xl font-semibold">
           {data?.name}
         </Link>
         <div className="flex justify-between items-center">
           <p>Rp {data?.price}</p>
           <div className="flex h-[2rem]">
-            <button className="w-20 border-[1px] rounded-full flex justify-center items-center bg-[#DE5959] text-white" onClick={() => router.push("/shop")}>
+            <Link href={"product/detail"} className="w-20 border-[1px] rounded-full flex justify-center items-center bg-[#DE5959] text-white">
               <Icon icon="material-symbols:edit" width={25} className="mr-0.5" />
               Edit
-            </button>
-            {/* <div className="flex justify-evenly border-[1px] items-center w-[4rem] bg-[#DE5959] text-white">
-                     <div className="">
-                        <p>{countCart}</p>
-                     </div>
-                     <Icon
-                        icon="material-symbols:shopping-cart-outline-rounded"
-                        width={20}
-                     />
-                  </div> */}
+            </Link>
           </div>
         </div>
       </div>
